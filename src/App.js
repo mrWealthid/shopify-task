7d94b3db8f31c7bf297f3d19e806d25483bbd80b
Wealth Iduwe <wealthiduwe@gmail.com>
2021-09-20 19:40:59 +0100

    added all features

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar";
+import Shopify from "./components/Shopify";
+
+function App() {
+  return (
+    <div className="App min-h-screen">
+      <Navbar />
+      <Shopify />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { FaShopify } from "react-icons/fa";
+
+const Navbar = () => {
+  return (
+    <div className="bg-gray-700 text-white py-6">
+      <div className="w-10/12 mx-auto">
+        <div className="text-lg flex items-center gap-2">
+          <span>
+            <FaShopify
+              color="#bae26a"
+              className="w-6 h-6 md:w-8 p-1 shadow-xl animate-bounce md:h-8 object-fit bg-white rounded-full"
+            />
+          </span>
+          <span> Shopify Careers</span>
+        </div>
+        {/* <p className="text-gray-700 pl-8">Wealth's App</p> */}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
